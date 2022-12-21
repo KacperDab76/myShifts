abd1b4e378e12ec6227b4742f42fbc1aed49ebd0
Kacper Dabrowski <kacper76dab@gmail.com>
2022-12-21 16:11:19 +0000

    dates and day name s

--- shifts.js
+++ shifts.js
@@ -14,8 +14,10 @@ const shiftKey = {
 	"D1": "8.00-16.00",
 	"D2": "8.00-15.00",
 	"D3": "10.00-18.00",
-	"P1": "Priorytet 1",
-	"P2": "Priorytet 2"
+	"P1": "Rest Day: P1",
+	// "P1": "Priorytet 1",
+	"P2": "Rest Day: P2"
+	// "P2": "Priorytet 2"
 	};
 
     const shiftsWeek = [
@@ -30,6 +32,15 @@ const shiftKey = {
     ];
     const startingDate =  "2021-10-03";
 
+    const weekDays = [
+        ["Sun","Sunday"],
+        ["Mon","Monday"],
+        ["Tue", "Tuesday"],
+        ["Wed","Wednesday"],
+        ["Thu","Thursday"],
+        ["Fri","Friday"],
+        ["Sat","Saturday"]
+    ];
 /**
  * Function checks today's date and sets up input:date element to it
  */
@@ -56,14 +67,21 @@ function showShift(day) {
     let days = diff/(1000*60*60*24);
     let weeks = Math.floor(days/7);
     let weekNo = weeks%8;
+
+    const dayOfWeek=day0.getDay();
+    // day of week , Sun is 0 [0] is short [1] is long
+    const nameOfDay = weekDays[dayOfWeek];
     // let key="P2";
     // document.getElementById("info").innerHTML += shiftKey[key];
     // document.getElementById("info").innerHTML += "<br>"+days+" : "+weeks+" : "+weekNo;
-    document.getElementById("info").innerHTML = "<br>"+day;
+    document.getElementById("info").innerHTML = "<br>"+day+" "+nameOfDay[1];
 
-    const dayOfWeek=day0.getDay();
+ 
     
     let table = "<table>";
+    table += "<colgroup><col class='names' /><col /></colgroup>";
+    table += "<tr><th>Name</th><th>"+day+"<br>"+nameOfDay[0]+"<tr>";
+    table += "</tr>";
     for (i=0;i<shiftsWeek.length;i++){
         table += "<tr>";
         // worker moved by week number 
@@ -92,14 +110,31 @@ function showShift(day) {
         let days = diff/(1000*60*60*24);
         let weeks = Math.floor(days/7);
         let weekNo = weeks%8;
+        const weekDiv = document.getElementById("week");
         // let key="P2";
         // document.getElementById("info").innerHTML += shiftKey[key];
         // document.getElementById("info").innerHTML += "<br>"+days+" : "+weeks+" : "+weekNo;
-        document.getElementById("info").innerHTML = "<br>"+day;
+        weekDiv.innerHTML = "<br>"+day;
     
         const dayOfWeek=day0.getDay();
+        // day of week , Sun is 0 [0] is short [1] is long
+        
+        let sunday = new Date();
+        sunday.setDate(day0.getDate()-dayOfWeek);
         
         let table = "<table>";
+        // create headers
+        table += "<tr>";
+        table += "<th>Name</th>";
+        for (let i=0;i<7;i++){
+            let dateForDay = new Date();
+            dateForDay.setDate(sunday.getDate()+i);                
+            table += "<th>";
+            table += weekDays[i][1]+"<br>";
+            table += dateForDay.toISOString().slice(0,10);
+            table += "</th>";
+        }
+    	table += "</tr>";
         for (i=0;i<shiftsWeek.length;i++){
             table += "<tr>";
             // worker moved by week number 
@@ -120,9 +155,81 @@ function showShift(day) {
             table += "</tr>";
         }
         table += "</table>";
-        document.getElementById("week").innerHTML += table;
+        weekDiv.innerHTML += table;
+        weekDiv.style.display = "block";
+
 }
+ 
+
+/**function 
+ * shows whole week or day in color or black and white
+ * in given name element
+ */
+let tableColors = ["BW","C1"];
+//BW - black and whites
+//mode 0: day, 1: week
+function showEverything(day,divId,color=1,mode=1) {
     
+    /**the thing */
+        const start = new Date(startingDate);
+        let day0 = new Date(day);
+        let diff = day0.getTime() - start.getTime();
+        let days = diff/(1000*60*60*24);
+        let weeks = Math.floor(days/7);
+        let weekNo = weeks%8;
+        const targetDiv = document.getElementById(divId);
+        // let key="P2";
+        // document.getElementById("info").innerHTML += shiftKey[key];
+        // document.getElementById("info").innerHTML += "<br>"+days+" : "+weeks+" : "+weekNo;
+    
+        const dayOfWeek=day0.getDay();
+        // day of week , Sun is 0 [0] is short [1] is long
+        const nameOfDay = weekDays[dayOfWeek];
+        targetDiv.innerHTML = "<br>"+day+nameOfDay[1];   // 0 is short 1 is long
+
+        let sunday = new Date();
+        sunday.setDate(day0.getDate()-dayOfWeek);
+        
+        let table = "<table>";
+        // create headers
+        table += "<tr>";
+        table += "<th>Name</th>";
+        for (let i=0;i<7;i++){
+            let dateForDay = new Date();
+             
+            if(mode == 1) {
+                dateForDay.setDate(sunday.getDate()+i);                
+                table += "<th>";
+                table += weekDays[i][1]+"<br>";
+                table += dateForDay.toISOString().slice(0,10);
+                table += "</th>";
+            }    
+        }
+    	table += "</tr>";
+        for (i=0;i<shiftsWeek.length;i++){
+            table += "<tr>";
+            // worker moved by week number 
+            let index = i-weekNo;
+            if (index < 0){
+                index = 8+index;
+            }
+            table += "<td>"+ workers[index]+"</td>";
+            for (j=0;j<shiftsWeek[i].length;j++){
+                if (j == dayOfWeek){
+                    table += "<td class='today "+shiftsWeek[i][j]+"'>";
+                } 
+                else {
+                    table += "<td class='"+shiftsWeek[i][j]+"'>";
+                }          
+                table += shiftKey[shiftsWeek[i][j]]+"</td>";
+            }
+            table += "</tr>";
+        }
+        table += "</table>";
+        targetDiv.innerHTML += table;
+        targetDiv.style.display = "block";
+
+}
 function newDay() {
     let day = document.getElementById("shiftDate").value;
     showShift(day);
