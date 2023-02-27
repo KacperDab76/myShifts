8b1fddf3600414354850290cb6f974a02115d571
Kacper Dabrowski <kacper76dab@gmail.com>
2023-02-27 15:49:53 +0000

    info for project

--- shifts.js
+++ shifts.js
@@ -29,16 +29,28 @@ const shiftKey = {
         ["R", "P2", "P1", "L1", "L2", "L2", "L4"]
     ];
     const startingDate =  "2021-10-03";
+// keeps date of what is shown
+let showDate;
 
+function dateToString(date){
+    return  date.toISOString().slice(0,10);
+
+}
 /**
  * Function checks today's date and sets up input:date element to it
  */
 function start()
 {
     const today = new Date();
+    showDate = dateToString(today);
     document.getElementById("shiftDate").value = today.toISOString().slice(0,10);
 
     showShift(today.toISOString().slice(0,10));
+
+    document.getElementById("shiftDate").addEventListener("change",setNewDay);
+    document.getElementById("black").addEventListener("click",showInBlack);
+    document.getElementById("color1").addEventListener("click",showInColor1);
+
 }
 
 // getDay() retruns day of week 0-6
@@ -127,10 +139,100 @@ function calcWeekNo(day0){
         }
         table += "</table>";
         document.getElementById("week").innerHTML = table;
+        document.getElementById("week").style.display = "block";
+
+}
+
+
+/**function 
+ * shows whole week or day in color or black and white
+ * in given name element
+ */
+let tableColors = ["BW","C1"];
+//BW - black and whites
+//mode 0: day, 1: week
+function showEverything(divId,day=showDate,color=1,mode=1) {
+    
+    /**the thing */
+        let day0 = new Date(day);
+        
+        let weekNo = calcWeekNo(day0); // returns no of week
+        const targetDiv = document.getElementById(divId);
+        // let key="P2";
+        // document.getElementById("info").innerHTML += shiftKey[key];
+        // document.getElementById("info").innerHTML += "<br>"+days+" : "+weeks+" : "+weekNo;
+    
+        const dayOfWeek=day0.getDay();
+        // day of week , Sun is 0 [0] is short [1] is long
+        const nameOfDay = weekDays[dayOfWeek];
+        console.log("show me"+day0);
+        targetDiv.innerHTML = "<br>"+day+" : "+nameOfDay[1];   // 0 is short 1 is long
+
+        let sunday = new Date(day);
+        // console.log(sunday);
+        sunday.setDate(day0.getDate()-dayOfWeek);
+        // console.log(sunday);
+        
+        let table = "<table>";
+        // create headers
+        table += "<tr>";
+        table += "<th>Shift</th>";
+        let startLoop = 0;
+        let endLoop = 7;
+        for (let i=startLoop;i<endLoop;i++){
+            let dateForDay = new Date();
+            // console.log(i,startLoop,endLoop); 
+            if(mode == 1) {
+                dateForDay.setDate(sunday.getDate()+i);
+                date = dateToString(dateForDay);                
+                table += '<th class="t-button" onclick="newDay(\''+date+'\');">';
+                table += weekDays[i][1]+"<br>";
+                //table += date;
+                table += "</th>";
+            }    
+        }
+    	table += "</tr>";
+        for (i=0;i<shiftsWeek.length;i++){
+            table += "<tr>";
+            // worker moved by week number 
+            let index = i-weekNo;
+            if (index < 0){
+                index = 8+index;
+            }
+            table += "<td class='t-button'>"+ (i+1)+"</td>";
+            for (j=0;j<shiftsWeek[i].length;j++){
+                let colorClass = shiftsWeek[i][j];
+                if (color == 0) {
+                    colorClass = "blackWhite";
+                }
+                if (j == dayOfWeek){
+                    table += "<td class='today "+colorClass+"'>";
+                } 
+                else {
+                    table += "<td class='"+colorClass+"'>";
+                }          
+                table += shiftKey[shiftsWeek[i][j]]+"</td>";
+            }
+             
+            table += "</tr>";
+        }
+        table += "</table>";
+        targetDiv.innerHTML += table;
         targetDiv.style.display = "block";
 
 }
-    
+
+function setNewDay() {
+    let day = document.getElementById("shiftDate").value;
+    newDay(day);
+}
+function showInBlack() {
+    showEverything("week",showDate,0,1);
+}
+function showInColor1() {
+    showEverything("week",showDate,1,1);
+}
+
 /** Method triggered by form (button or change of date) */
 function newDay() {
     let day = document.getElementById("shiftDate").value;
