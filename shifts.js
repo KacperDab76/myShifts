5a72118ed93812a1c542751e5e89e185a71791f0
Kacper Dabrowski <kacper76dab@gmail.com>
2023-03-10 07:58:04 +0000

    cleanup of unused functions

--- shifts.js
+++ shifts.js
@@ -65,43 +65,6 @@ function start()
 
 }
 
-// getDay() retruns day of week 0-6
-
-/**function 
- * shows one day
- */
-
-function showShift(day) {
-    
-/**tryouts */
-    const start = new Date(startingDate);
-    let day0 = new Date(day);
-    let diff = day0.getTime() - start.getTime();
-    let days = diff/(1000*60*60*24);
-    let weeks = Math.floor(days/7);
-    let weekNo = weeks%8;
-    // let key="P2";
-    // document.getElementById("info").innerHTML += shiftKey[key];
-    // document.getElementById("info").innerHTML += "<br>"+days+" : "+weeks+" : "+weekNo;
-    document.getElementById("info").innerHTML = "<br>"+day;
-
-    const dayOfWeek=day0.getDay();
-    
-    let table = "<table>";
-    for (i=0;i<shiftsWeek.length;i++){
-        table += "<tr>";
-        // worker moved by week number 
-        let index = i-weekNo;
-        if (index < 0){
-            index = 8+index;
-        }
-        table += "<td>"+ workers[index]+"</td>";
-        table += "<td class='"+shiftsWeek[i][dayOfWeek]+"'>"+ shiftKey[shiftsWeek[i][dayOfWeek]]+"</td>";
-        table += "</tr>";
-    }
-    table += "</table>";
-    document.getElementById("info").innerHTML += table;
-}
 
 // funciton calculates week based on given date and number of weeks
 function calcWeekNo(day0,noOfWeeks){
@@ -114,129 +77,6 @@ function calcWeekNo(day0,noOfWeeks){
         return weekNo;
 }
 
-/**function 
- * shows whole week
- */
-
- function showShiftWeek(day) {
-    
-    /**tryouts */
-        
-        let day0 = new Date(day);
-        let weekNo = calcWeekNo(day0,8);
-        // let key="P2";
-        // document.getElementById("info").innerHTML += shiftKey[key];
-        // document.getElementById("info").innerHTML += "<br>"+days+" : "+weeks+" : "+weekNo;
-        document.getElementById("info").innerHTML = "<br>"+day;
-    
-        const dayOfWeek=day0.getDay();
-        let table = "<table>";
-        for (i=0;i<shiftsWeek.length;i++){
-            table += "<tr>";
-            // worker moved by week number 
-            let index = i-weekNo;
-            if (index < 0){
-                index = 8+index;
-            }
-            table += "<td>"+ workers[index]+"</td>";
-            for (j=0;j<shiftsWeek[i].length;j++){
-                if (j == dayOfWeek){
-                    table += "<td class='today "+shiftsWeek[i][j]+"'>";
-                } 
-                else {
-                    table += "<td class='"+shiftsWeek[i][j]+"'>";
-                }          
-                table += shiftKey[shiftsWeek[i][j]]+"</td>";
-            }
-            table += "</tr>";
-        }
-        table += "</table>";
-        document.getElementById("week").innerHTML = table;
-        document.getElementById("week").style.display = "block";
-
-}
-
-
-/**function 
- * shows whole week or day in color or black and white
- * in given name element
- */
-let tableColors = ["BW","C1"];
-//BW - black and whites
-//mode 0: day, 1: week
-function showEverything(divId,day=showDate,color=1,mode=1) {
-    
-    /**the thing */
-        let day0 = new Date(day);
-        let noOfWeeks = shiftsWeek.length;
-        // console.log("weeks "+noOfWeeks);
-        let weekNo = calcWeekNo(day0,noOfWeeks); // returns no of week
-        const targetDiv = document.getElementById(divId);
-        // let key="P2";
-        // document.getElementById("info").innerHTML += shiftKey[key];
-        // document.getElementById("info").innerHTML += "<br>"+days+" : "+weeks+" : "+weekNo;
-    
-        const dayOfWeek=day0.getDay();
-        // day of week , Sun is 0 [0] is short [1] is long
-        const nameOfDay = weekDays[dayOfWeek];
-        // console.log("show me"+day0);
-        targetDiv.innerHTML = "<br>"+day+" : "+nameOfDay[1];   // 0 is short 1 is long
-
-        let sunday = new Date(day);
-        // console.log(sunday);
-        sunday.setDate(day0.getDate()-dayOfWeek);
-        // console.log(sunday);
-        
-        let table = "<table>";
-        // create headers
-        table += "<tr>";
-        table += "<th>Worker</th>";
-        let startLoop = 0;
-        let endLoop = 7;
-        for (let i=startLoop;i<endLoop;i++){
-            let dateForDay = new Date();
-            // console.log(i,startLoop,endLoop); 
-            if(mode == 1) {
-                dateForDay.setDate(sunday.getDate()+i);
-                date = dateToString(dateForDay);                
-                table += '<th class="t-button" onclick="newDay(\''+date+'\');">';
-                table += weekDays[i][1]+"<br>";
-                table += date;
-                table += "</th>";
-            }    
-        }
-    	table += "</tr>";
-        for (i=0;i<shiftsWeek.length;i++){
-            table += "<tr>";
-            // worker moved by week number 
-            let index = i-weekNo;
-            if (index < 0){
-                index = 8+index;
-            }
-            
-
-            table += "<td class='t-button'>"+ workers[index]+"</td>";
-            for (j=0;j<shiftsWeek[i].length;j++){
-                let colorClass = shiftsWeek[i][j];
-                if (color == 0) {
-                    colorClass = "blackWhite";
-                }
-                if (j == dayOfWeek){
-                    table += "<td class='today "+colorClass+"'>";
-                } 
-                else {
-                    table += "<td class='"+colorClass+"'>";
-                }          
-                table += shiftKey[shiftsWeek[i][j]]+"</td>";
-            }
-             
-            table += "</tr>";
-        }
-        table += "</table>";
-        targetDiv.innerHTML += table;
-        targetDiv.style.display = "block";
-
-}
 
 // I need 2 functions
 // or more >
@@ -343,7 +183,7 @@ function showWeek(day,divId){
     // targetDiv.style.display = "block";
 }
 
-// extrakts 1 day from workers table
+// extracts 1 day from workers table
 // numOfDay is 0-6 days of week
 function getDay(week,numOfDay){
     const dayNum = numOfDay+1; // 0 is for worker
